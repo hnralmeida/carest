4a10df3da058ee9b8bd6a50ee0899b7fb4852387
Paulo368 <pauloviniciushuwer@hotmail.com>
2025-03-07 19:04:01 -0300

    Update ProdutoBalanca.java

--- backend/carest/src/main/java/com/les/carest/model/ProdutoBalanca.java
+++ backend/carest/src/main/java/com/les/carest/model/ProdutoBalanca.java
@@ -3,6 +3,12 @@ package com.les.carest.model;
 import jakarta.persistence.*;
 import java.util.Date;
 
+import com.probuild.backend.domain.interfaces.Pronomes;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+    
+
 @Entity
 @DiscriminatorValue("PRODUTO BALANÇA") // Define o valor no campo "tipo"
 public class ProdutoBalanca extends Produto {
@@ -10,19 +16,4 @@ public class ProdutoBalanca extends Produto {
     @Temporal(TemporalType.TIMESTAMPBalanca(S))
     private Date data;
 
-    public ProdutoBalanca() {
-    }
-
-    public Produto (String nome, double valor, Date data) {
-        super(nome, valor);
-        this.data = data;
-    }
-
-    public Date getData() {
-        return data;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
-    }
 }
