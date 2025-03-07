7b22088194753c68604bc2036e33f7ae705da7d4
Paulo368 <pauloviniciushuwer@hotmail.com>
2025-03-07 19:07:15 -0300

    Update CompraFornecedor.java

--- backend/carest/src/main/java/com/les/carest/model/CompraFornecedor.java
+++ backend/carest/src/main/java/com/les/carest/model/CompraFornecedor.java
@@ -1,70 +1,20 @@
 package com.les.carest.model;
 
 import java.util.Date;
+import com.probuild.backend.domain.interfaces.Pronomes;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
 
 @Entity
 public class CompraFornecedor {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private int id;
     @ManyToOne
     private Fornecedor fornecedor_id;
     private double valor;
     private Date vencimento;
     private String descricao;
-
-    public CompraFornecedor(int id, Fornecedor fornecedor_id, double valor, Date vencimento, String descricao) {
-        this.id = id;
-        this.fornecedor_id = fornecedor_id;
-        this.valor = valor;
-        this.vencimento = vencimento;
-        this.descricao = descricao;
-    }
-
-    public CompraFornecedor(Fornecedor fornecedor_id, double valor, Date vencimento, String descricao) {
-        this.fornecedor_id = fornecedor_id;
-        this.valor = valor;
-        this.vencimento = vencimento;
-        this.descricao = descricao;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Fornecedor getFornecedor_id() {
-        return fornecedor_id;
-    }
-
-    public void setFornecedor_id(Fornecedor fornecedor_id) {
-        this.fornecedor_id = fornecedor_id;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public Date getVencimento() {
-        return vencimento;
-    }
-
-    public void setVencimento(Date vencimento) {
-        this.vencimento = vencimento;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
