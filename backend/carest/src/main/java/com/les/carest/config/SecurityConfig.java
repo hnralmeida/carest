53fa225d8dca6bcae5a25d1e187076e3631165f2
Paulo368 <pauloviniciushuwer@hotmail.com>
2025-03-21 21:12:38 -0300

    update backend

--- backend/carest/src/main/java/com/les/carest/config/SecurityConfig.java
+++ backend/carest/src/main/java/com/les/carest/config/SecurityConfig.java
@@ -2,14 +2,34 @@ package com.les.carest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usuario").permitAll() // Libera o endpoint /usuario
+                        .anyRequest().permitAll() // Exige autenticação para todos os outros endpoints
+                )
+                .httpBasic(); // Habilita autenticação básica (usuário e senha)
+        return http.build();
     }
 }
